e40974ea6fb50a3daa0a3316dd68a6f211601dd3
Abhinav-Modi <abhinavmodi07@gmail.com>
2022-12-16 10:22:41 +0530

    modified files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+	return <div>App</div>;
+};
 
 export default App;
